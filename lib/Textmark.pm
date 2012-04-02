16350da0cc8ba953d40cd0ae0ac102f43fdac93a
Robert Grimes <rmzgrimes@gmail.com>
2012-04-02 02:49:46 -0500

    Added the initial h2xs skeleton for this perl module.

---
+++ lib/Textmark.pm
@@ -0,0 +1,55 @@
+package Textmark;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Textmark - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Textmark;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Textmark, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Robert Grimes, E<lt>rmzgrimes@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Robert Grimes
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
