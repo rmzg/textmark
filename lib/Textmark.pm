9f6f638f7e2da6a668f9f9675a0a02b474c98e02
Robert Grimes <rmzgrimes@gmail.com>
2012-04-02 22:18:13 -0500

    First implementation of list parsing. Added a test file for lists.

--- lib/Textmark.pm
+++ lib/Textmark.pm
@@ -3,13 +3,149 @@ package Textmark;
 use strict;
 use warnings;
 
+use Exporter qw/import/;
+
+our @EXPORT_OK = qw/textmark/;
+
 our $VERSION = '0.01';
 
+my $NL = qr/\r?\n/;
+
+my $LIST_MARKER = qr/(\*|#)/;
+
+# Formats  text!
+# Returns HTML!
+sub textmark {
+	my( $text ) = @_;
+
+	# Remove any tailing newline so we don't duplicate it
+	chomp $text;
+	# When we ensure the text ends with a newline to make parsing simpler.
+	$text .= "\n"; 
+
+	my $output = ""; #The text we return
+	my @list_struct;
+
+	while( local $_ = get_line( \$text ) ) {
+
+		if( s/^\s*$LIST_MARKER\s+// ) {
+			push @list_struct, [ $1, $_ ];
+			my $current_list_level = \@list_struct;
+			my @current_list_level_stack;
+
+			my $current_depth = 1;
+
+			while( local $_ = get_line( \$text ) ) {
+
+				my $depth = 0;
+				$depth++ while s/^(\s*$LIST_MARKER\s+)//g;
+
+				my $row_item = [ $2, $_ ];
+
+				if( $depth < 1 ) { 
+					unget_line( $_ );
+					last;
+				}
+				elsif( $depth == $current_depth ) {
+					push @{$current_list_level}, $row_item;
+				}
+				elsif( $depth > $current_depth ) {
+					push @current_list_level_stack, $current_list_level;
+					$current_list_level = $current_list_level->[-1][2] ||= [];
+					$current_depth++;
+
+					push @{$current_list_level}, $row_item;
+				}
+				elsif( $depth < $current_depth ) {
+					$current_list_level = pop @current_list_level_stack
+						for( 1 .. ( $current_depth - $depth ) );
+					$current_depth = $depth;
+					push @{$current_list_level}, $row_item;
+				}
+				else {
+					die "How exactly did we get here?";
+				}
+
+			}
+
+			#$output .= build_list( \@list_struct );
+			my $t = build_list( \@list_struct );
+
+			#print "\n-----\n$t\n------\n";
+
+			$output .= $t;
+
+		}
+	}
+
+	return $output;
+}
+
+
+my @line_buffer;
+sub unget_line {
+	push @line_buffer, $_[0];
+}
+sub get_line {
+	my( $text_ref ) = @_;
+	
+	if( @line_buffer ) { return pop @line_buffer; }
+
+	if( $$text_ref =~ s/^(.+)$NL// ) { 
+		return $1;
+	}
+		
+	return;
+}
+
+
+############################################ 
+# HTML GENERATION
+############################################ 
+
+sub build_list {
+	my $list = shift;
+	my $indent = shift || 0;
+
+	my $output;
+	my $last_type = '';
+
+	for( @$list ) { 
+
+		if( $_->[0] eq '*' and $last_type ne '*' ) {
+			$output .= ( "\t" x $indent ) . "<ul>\n";
+			$last_type = '*';
+		}
+		elsif( $_->[0] eq '#' and $last_type ne '#' ) {
+			$output .= ( "\t" x $indent ) . "<ol>\n";
+			$last_type = '#';
+		}
+
+		$output .= ( "\t" x $indent ) . "<li><p>$_->[1]</p>";
+
+		if( $_->[2] ) {
+			$output .= "\n" . build_list( $_->[2], $indent + 1 );
+			$output .= "\t" x $indent;
+		}
+
+		$output .= "</li>\n";
+	}
+
+	if( $last_type eq '*' ) { 
+		$output .= ( "\t" x $indent ) . "</ul>\n";
+	}
+	elsif( $last_type eq '#' ) {
+		$output .= ( "\t" x $indent ) . "</ol>\n";
+	}
+	else {
+		die "Last type definitely should be set here!";
+	}
+
+	return $output;
+}
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
-
 =head1 NAME
 
 Textmark - Perl extension for blah blah blah
---
+++ t/01-lists.t
@@ -0,0 +1,140 @@
+use strict;
+use Test::More;
+
+use Textmark qw/textmark/;
+
+my $input = <<EOF;
+* a list
+* of
+* items
+* here
+EOF
+
+my $output = <<EOF;
+<ul>
+<li><p>a list</p></li>
+<li><p>of</p></li>
+<li><p>items</p></li>
+<li><p>here</p></li>
+</ul>
+EOF
+
+is( textmark( $input ), $output );
+
+
+$input = <<EOF;
+* more complicated
+* lists
+* * containing a sub list
+* * with items
+* * * and another sub
+* * back to level
+* at first again
+* blah
+* * second sub list
+* * with two items
+* and a final element
+EOF
+
+$output = <<EOF;
+<ul>
+<li><p>more complicated</p></li>
+<li><p>lists</p>
+	<ul>
+	<li><p>containing a sub list</p></li>
+	<li><p>with items</p>
+		<ul>
+		<li><p>and another sub</p></li>
+		</ul>
+	</li>
+	<li><p>back to level</p></li>
+	</ul>
+</li>
+<li><p>at first again</p></li>
+<li><p>blah</p>
+	<ul>
+	<li><p>second sub list</p></li>
+	<li><p>with two items</p></li>
+	</ul>
+</li>
+<li><p>and a final element</p></li>
+</ul>
+EOF
+
+is( textmark( $input ), $output );
+
+$input = <<EOF;
+* testing rapid
+* * decreases
+* * * and increases
+* * * in
+* levels
+EOF
+
+$output = <<EOF;
+<ul>
+<li><p>testing rapid</p>
+	<ul>
+	<li><p>decreases</p>
+		<ul>
+		<li><p>and increases</p></li>
+		<li><p>in</p></li>
+		</ul>
+	</li>
+	</ul>
+</li>
+<li><p>levels</p></li>
+</ul>
+EOF
+
+is( textmark( $input ), $output );
+
+$input = <<EOF;
+# Testing
+# ordered
+# lists
+EOF
+
+$output = <<EOF;
+<ol>
+<li><p>Testing</p></li>
+<li><p>ordered</p></li>
+<li><p>lists</p></li>
+</ol>
+EOF
+
+is( textmark( $input ), $output );
+
+
+$input = <<EOF;
+# multi
+# # level
+# # # ordered
+# # list
+# # items
+# being tested
+# here
+EOF
+
+$output = <<EOF;
+<ol>
+<li><p>multi</p>
+	<ol>
+	<li><p>level</p>
+		<ol>
+		<li><p>ordered</p></li>
+		</ol>
+	</li>
+	<li><p>list</p></li>
+	<li><p>items</p></li>
+	</ol>
+</li>
+<li><p>being tested</p></li>
+<li><p>here</p></li>
+</ol>
+EOF
+
+is( textmark( $input ), $output );
+
+done_testing();
+
